6f9c405c682b417f1485db27a1afde119afd1b6b
Ayotunde-Orintunsin <idowuorintunsin@outlook.com>
2024-06-29 02:48:51 +0100

    modification to the infrasturcture

--- launch_configuration_app_lc.tf
+++ launch_configuration_app_lc.tf
@@ -3,6 +3,7 @@ resource "aws_launch_configuration" "app_lc" {
   image_id      = var.ami
   instance_type = var.instance_type
   security_groups = [aws_security_group.app_sg.id]
+  key_name      = var.key_name
 
   lifecycle {
     create_before_destroy = true
--- launch_configuration_web_lc.tf
+++ launch_configuration_web_lc.tf
@@ -3,6 +3,7 @@ resource "aws_launch_configuration" "web_lc" {
   image_id      = var.ami
   instance_type = var.instance_type
   security_groups = [aws_security_group.web_sg.id]
+  key_name      = var.key_name
 
   lifecycle {
     create_before_destroy = true
